8c850b07c891eb1c51500a1dcd492eba60b8b3a7
donghizzle <dongheec.lee@gmail.com>
2019-05-19 19:43:56 -0400

    Update Week_1.py

--- Week_1.py
+++ Week_1.py
@@ -1,1 +1,11 @@
-
+'''
+Python Basics 
+  - is a high level language, which means it's optimized for reading by people instead of machines. 
+  - is interpreted, meaning it doesn't compile to machine-level code for it to run. 
+  - is dynamically-typed, meaning you do not need to declare the type of variable. 
+  - because there is no compilation step, Python can be used interactively. 
+  
+Python Functions 
+  - all of the optional parameters, the ones that you got default values for, need to come at the end of the function declaration. 
+  - you can assign a variable to a function
+'''
