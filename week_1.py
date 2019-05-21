da44241e7a7873d561a233f5f2d394a8bbafcce9
donghizzle <dongheec.lee@gmail.com>
2019-05-20 22:27:48 -0400

    Update week_1.py

--- week_1.py
+++ week_1.py
@@ -10,13 +10,13 @@ Python Functions
   - you can assign a variable to a function
   
 Python Strings 
-  - Python strings are unicode-based in Python 3 (suppots over a million diff characters). 
-    In Python 2, string are ASCII-based (1-256 different latin chars + 0-9 numericals). 
+  - Python strings are unicode-based in Python 3 (supports over a million diff characters). 
+    In Python 2, strings are ASCII-based (1-256 different latin chars + 0-9 numericals). 
     The transition to unicode was due to a need to support non-latin chars + mathematical operators as well. 
   - String manipulation is important for data cleaning...relevant functions are format and string. 
   
 Python Dates and Times
-  - The date time object has handy attributes to get the representative hour, day, seconds, etc.
+  - The datetime object has handy attributes to get the representative hour, day, seconds, etc.
   - You might want to look for any five day span of time where sales were highest, and flag that for follow up --> where datetime's time deltas come into use.
 
 Python Objects + map()
@@ -100,3 +100,86 @@ digits = '0123456789'
 correct_answer = [a+b+c+d for a in lowercase for b in lowercase for c in digits for d in digits]
 
 correct_answer[:50] # Display first 50 ids
+
+
+######## numpy as np #############
+import numpy as np
+
+mylist = [1, 2, 3]
+x = np.array(mylist)
+x
+# array([1, 2, 3])
+
+
+m = np.array([[7, 8, 9], [10, 11, 12]]) #can pass in multiple lists 
+m
+#array([[ 7,  8,  9], [10, 11, 12]])
+
+
+m.shape #Use the shape method to find the dimensions of the array. (rows, columns)
+#(2, 3)
+
+
+
+#arange returns evenly spaced values within a given interval.
+n = np.arange(0, 30, 2) # start at 0 count up by 2, stop before 30
+n
+#array([ 0,  2,  4,  6,  8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28])
+
+
+
+
+#reshape returns an array with the same data with a new shape.
+n = n.reshape(3, 5) # reshape array to be 3x5
+n
+'''
+array([[ 0,  2,  4,  6,  8],
+       [10, 12, 14, 16, 18],
+       [20, 22, 24, 26, 28]])
+'''
+
+
+
+#linspace returns evenly spaced numbers over a specified interval.
+o = np.linspace(0, 4, 9) # return 9 evenly spaced values from 0 to 4
+o
+#array([ 0. ,  0.5,  1. ,  1.5,  2. ,  2.5,  3. ,  3.5,  4. ])
+
+
+
+#resize changes the shape and size of array in-place.
+o.resize(3, 3)
+o
+'''
+array([[ 0. ,  0.5,  1. ],
+       [ 1.5,  2. ,  2.5],
+       [ 3. ,  3.5,  4. ]])
+'''
+
+
+
+#eye returns a 2-D array with ones on the diagonal and zeros elsewhere.
+np.eye(3)
+'''
+array([[ 1.,  0.,  0.],
+       [ 0.,  1.,  0.],
+       [ 0.,  0.,  1.]])
+'''
+#ones returns a new array of given shape and type, filled with ones.
+np.ones((3, 2))
+'''
+array([[ 1.,  1.],
+       [ 1.,  1.],
+       [ 1.,  1.]])
+'''
+
+#Create an array using repeating list (or see np.tile)
+np.array([1, 2, 3] * 3)
+#array([1, 2, 3, 1, 2, 3, 1, 2, 3])
+
+
+
+
+#Repeat elements of an array using repeat.
+np.repeat([1, 2, 3], 3)
+#array([1, 1, 1, 2, 2, 2, 3, 3, 3])
